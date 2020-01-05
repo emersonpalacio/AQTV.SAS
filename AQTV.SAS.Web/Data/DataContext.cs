a8e864523288f25c8d0471236a214b25520bd9ce
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-01-05 14:11:09 -0500

    InitialDb

---
+++ AQTV.SAS.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using AQTV.SAS.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AQTV.SAS.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):  base(options)
+        {
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+
+    }
+}
--- AQTV.SAS.Web/Startup.cs
+++ AQTV.SAS.Web/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AQTV.SAS.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,6 +33,9 @@ namespace AQTV.SAS.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg => {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
