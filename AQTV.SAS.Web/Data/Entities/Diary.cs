2567d18b1d14375228dfb5651c48eaeb0d3aca3f
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-01-05 15:09:20 -0500

    FinallyEntities

---
+++ AQTV.SAS.Web/Data/Entities/Diary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AQTV.SAS.Web.Data.Entities
+{
+    public class Diary
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime DateLocal => Date.ToLocalTime();
+
+        public string Remarks { get; set; }
+
+        [Display(Name = "Is Available?")]
+        public bool IsAvailable { get; set; }
+
+    }
+}
---
+++ AQTV.SAS.Web/Data/Entities/ProductImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AQTV.SAS.Web.Data.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string ImageUrl { get; set; }
+
+        // TODO: Change the path when publish
+        public string ImageFullPath => $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+
+    }
+}
