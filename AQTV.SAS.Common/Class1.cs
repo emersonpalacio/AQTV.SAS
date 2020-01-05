3ae2e5038ba364b0cb853d483241dec290e20b19
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-01-05 12:04:38 -0500

    InitialWeb

---
+++ AQTV.SAS.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AQTV.SAS.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ AQTV.SAS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AQTV.SAS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
